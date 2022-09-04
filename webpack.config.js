5549966cc8979c774a8ad0bde9e43209c108919b
bbawuah <brianbawuah@icloud.com>
2022-09-04 19:11:25 +0200

    add tailwind to boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,12 @@ module.exports = {
             loader: 'css-loader'
           },
           {
-            loader: 'postcss-loader'
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-nested'], ['tailwindcss'], ['autoprefixer']]
+              }
+            }
           },
           {
             loader: 'sass-loader'
